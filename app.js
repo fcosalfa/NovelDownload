00b24e90718cbeca6d7615e6af8c284f4c9263cb
fcosalfa <fcosalfa@gmail.com>
2023-04-19 17:46:57 +0700

    Add files via upload
    Fix max length of the nextpage's url

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const TinyURL = require('tinyurl');
 
 // Define the URL of the website you want to crawl
 const baseUrl = 'https://novelfull.com';
 
 // Define the first chapter 
-const url = 'https://novelfull.com/astral-pet-store/chapter-1.html';
+const url = 'https://novelfull.com/i-am-loaded-with-passive-skills/chapter-1.html';
 
 let TotalChap = 0;
 let content;
@@ -14,54 +15,64 @@ let content;
 function crawlPage(url) {
 	// Make an HTTP request to the website
 	request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
+	    if (!error && response.statusCode == 200) {
 
-    	content = "";
+	    	// Reset content for the new chapter
+	    	content = "";
 
-      	// Load the HTML into cheerio
-      	const $ = cheerio.load(html);
+	      	// Load the HTML into cheerio
+	      	const $ = cheerio.load(html);
 
-		// Select Chapter title
-		const ChapterTitle = $('.chapter-text').text() + '\n'; 
-		content = '# ' + content + ChapterTitle;
+			// Select Chapter title
+			const ChapterTitle = $('.chapter-text').text() + '\n'; 
+			content = '# ' + content + ChapterTitle;
 
-		// Select all p elements
-		const paragraphs = $('#chapter-content p');
+			// Select all p elements
+			const paragraphs = $('#chapter-content p');
 
-		// Iterate over the elements
-		paragraphs.each((i, element) => {
-		  	// Get the text content of the element
-		  	const text = $(element).text();
-          	content = content + text + '\n';
-		});
+			// Iterate over the elements
+			paragraphs.each((i, element) => {
+			  	// Get the text content of the element
+			  	const text = $(element).text();
+	          	content = content + text + '\n';
+			});
 
-		// Write text to the file
-		fs.appendFile('new-file.txt', content, {encoding: 'utf8'}, (err) => {
-			if (err) throw err;
-		});
+			// Write text to the file
+			fs.appendFile('new-file.txt', content, {encoding: 'utf8'}, (err) => {
+				if (err) throw err;
+			});
 
-		// Look for a link to the next page
-		const nextPageLink = $('#next_chap');
+			// Look for a link to the next page
+			const nextPageLink = $('#next_chap');
 
-		// If there is a link to the next page
-		if (nextPageLink.length > 0) {
+			// If there is a link to the next page
+			if (nextPageLink.length > 0) {
 
-			// Get the href attribute of the link
-			const nextPageUrl = nextPageLink.attr('href');
+				// Get the href attribute of the link
+				const nextPageUrl = nextPageLink.attr('href');
 
-			// Chapter counter
-			TotalChap = TotalChap + 1;
+				// Chapter counter
+				TotalChap = TotalChap + 1;
+				console.log(TotalChap);
 
-			console.log(TotalChap);
+				// Completed the URL
+				const Nextpage = baseUrl + nextPageUrl;
 
-			// Crawl the next page
-		    crawlPage(baseUrl + nextPageUrl);
-			
-      	}
-    } else {
-    	console.log(error);
-    	console.log(url);
-    }
+				// Check the length of the URL
+				if(Nextpage.length > 120){
+					// Shorten URL
+					TinyURL.shorten(Nextpage, function(nextUrl, err) {
+						crawlPage(nextUrl);
+					});
+				} else {
+					crawlPage(Nextpage);
+				}
+	      	}
+	    }
+	    else {
+	    	console.log(error);
+	    	console.log(url);
+	    }
   });
 }
 
